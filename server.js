3922993272d750a5fdcde4eb922a758deebf6ffe
Mahfudin98 <mahfudinnawawi@gmail.com>
2025-02-26 17:23:12 +0700

    fix server

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ app.use(express.json());
 
 app.post("/webhook", async (req, res) => {
   try {
-    const response = await axios.get("http://192.168.1.225/trigger");
+    const response = await axios.get(
+      "https://2da6-2001-448a-3003-2faa-a92a-de6f-6915-2093.ngrok-free.app/trigger"
+    );
     res.json({ success: true, data: response.data });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
